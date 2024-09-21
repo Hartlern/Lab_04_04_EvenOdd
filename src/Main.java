34e91dd4d3b762502d88cd76b0c99cf49414a6e3
Hartlern <hartlern@mail.uc.edu>
2024-09-20 22:32:53 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int num = 11;
+//modulo % sees if it fits evenly into it. remainder is the 0.
+        if (num % 2 == 0) {
+
+            // If remainder is zero then this number is even
+            System.out.println("Entered Number is Even");
+        } else {
+
+            // odd
+
+            System.out.println("Entered Number is Odd");
+
+        }
+    }
+}
